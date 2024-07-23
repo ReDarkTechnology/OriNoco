a89c5be95fe0c6e0eb546cf1f314cc771e8fe86a
ReDark Technology <namarahasia12345@gmail.com>
2024-07-23 23:57:21 +0700

    Updated data system

--- OriNoco/Data/ChartData.cs
+++ OriNoco/Data/ChartData.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OriNoco.Data
 {
     [Serializable]
     public class ChartData
     {
+        public ChartInfoData info = new();
+        public List<NoteData> notes = [];
     }
 }
---
+++ OriNoco/Data/ChartInfoData.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace OriNoco.Data
+{
+    [Serializable]
+    public class ChartInfoData
+    {
+        public string? levelCodename = "untitled_level";
+        public string? audioCodename = "Unnamed";
+
+        public string? displayName = "Untitled";
+        public string? chartCreator = "Unknown";
+        public string? levelComposer = "Unknown";
+        public float levelDifficulty = 0f;
+        public ComposerSocials[] composerSocials = [];
+        public LevelSets levelSet = LevelSets.Community;
+
+        public float speed = 1f;
+        public float syncOffset;
+        public float syncOffsetAnim = -1;
+
+        public float noteAnimationTimeDelay = 1f;
+        public float noteAnimationSpeed = 0.3f;
+        public float orthographicSize = 5f;
+
+        public ColorF globalSpriteColor = new ColorF(1f, 1f, 1f);
+        public bool invertedColors;
+
+        public ColorF bgColor;
+        public ColorF lineColor;
+        public ColorF particleColor;
+        public ColorF gradientColor;
+        public ColorF textColor;
+        public ColorF firefliesColor;
+
+        public bool isLockLevel;
+        public LockType lockMode;
+        public int requiredAmount;
+    }
+
+    [Serializable]
+    public struct ComposerSocials
+    {
+        public string url;
+        public string path;
+    }
+
+    public enum LockType
+    {
+        RankingScore,
+        Score,
+        Accuracy
+    }
+
+    public enum LevelSets
+    {
+        Chapter1,
+        Chapter2,
+        Chapter3,
+        Chapter4,
+        SideStory1,
+        SideStory2,
+        Single,
+        Community,
+        Legacy,
+        Test
+    }
+}
